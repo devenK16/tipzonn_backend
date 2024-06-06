77231381de0237441546c85e33236121dc3d3334
devenK16 <devenkhobragade007@gmail.com>
2024-06-06 09:31:37 +0530

    test razorpay key added

--- routes/payment.js
+++ routes/payment.js
@@ -3,8 +3,8 @@ const Razorpay = require('razorpay');
 const router = express.Router();
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID_TEST,
+  key_secret: process.env.RAZORPAY_KEY_SECRET_TEST,
 });
 
 router.post('/create-order', async (req, res) => {
