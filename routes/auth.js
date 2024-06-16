e7162dc092682e898851bcddfdd62be82ddef966
devenK16 <devenkhobragade007@gmail.com>
2024-06-16 12:21:34 +0530

    change password feat added

--- routes/auth.js
+++ routes/auth.js
@@ -89,4 +89,30 @@ router.post('/signin', async (req, res) => {
   }
 });
 
+// Change Password
+router.put('/changepassword', auth, async (req, res) => {
+  const { newPassword } = req.body;
+  const userId = req.user.id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // const isMatch = await bcrypt.compare(currentPassword, user.password);
+    // if (!isMatch) {
+    //   return res.status(400).json({ message: 'Current password is incorrect' });
+    // }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.json({ message: 'Password updated successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
